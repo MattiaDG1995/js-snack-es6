e13750efd1cd9236ca8597f420427debcc92ffdb
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-08 16:36:17 +0100

    end snack 2

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ let students = [
  
 
 
- let arrayStudenti = [
+ /* let arrayStudenti = [
     { name: 'Marco', id: 213, grades: 78 },
     { name: 'Paola', id: 110, grades: 96 },
     { name: 'Andrea', id: 250, grades: 48 },
@@ -71,7 +71,7 @@ let students = [
     { name: 'Luigi', id: 196, grades: 68 },
     { name: 'Piero', id: 102, grades: 50 },
     { name: 'Francesca', id: 120, grades: 84 },
-  ];
+  ]; */
 /* 
   let arrayNuovo = arrayStudenti.map( ( element ) => {
 
@@ -88,12 +88,61 @@ let students = [
 
  console.log(maxVoti) */
 
- let topStudenti = arrayStudenti.filter((element) => {
+ /* let topStudenti = arrayStudenti.filter((element) => {
     if( element.grades > 70 && element.id > 120){
         return true    
     }
  })
 
  console.log(topStudenti)
+ */
 
+
+
+
+/* Snack 3
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+ */
+
+/* let arrayBikes = [
+  { nome: 'shimano', peso: 20},
+  { nome: 'bmx', peso: 15}
+];
+
+let { nome, peso } = arrayBikes[1];
+console.log(nome, peso)
+
+document.querySelector('p').innerHTML = `La bici con perso minore è: ${nome}`;  */
+
+
+
+
+/* Snack4
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
  
+/* numberGenerator(1, 10)
+
+let arraySquadre = [
+  { nome: 'gubbio', punti: 0, falli: 0  },
+  { nome: 'cesano', punti: 0, falli: 0 },
+  { nome: 'tragliatella', punti: 0, falli: 0 },
+]; 
+
+function numberGenerator(min,max){
+  return parseInt(Math.floor(Math.random() * (max - min + 1) + min));
+};
+
+let arrayNuovo = arraySquadre.map( ( {punti, falli, nome} ) => {
+   
+  punti=numberGenerator(1, 10)  
+  falli=numberGenerator(1, 10)
+
+  return {falli, nome}
+
+})
+
+console.log(arrayNuovo)
+
+ */
